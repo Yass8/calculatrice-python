265bcf1b0811e8b9c37823b32164d95827fd01b0
yassir ali <aliyassir859@gmail.com>
2024-05-19 21:22:14 +0200

    Mise à jour et Réfactorisation du design de la calculatrice

--- script.py
+++ script.py
@@ -1,95 +1,82 @@
 import customtkinter
 
+# Classe principale de la calculatrice héritant de customtkinter.CTk
 class Calculator(customtkinter.CTk):
     def __init__(self):
         super().__init__()
 
+        # Configuration de la fenêtre principale
         self.title("Calculator")
-        self.geometry("300x470")
+        self.geometry("300x470") 
         self.resizable(False, False)
 
-        self.grid_rowconfigure((0, 1), weight=1)  # configure grid system
-        self.grid_columnconfigure((0, 1), weight=1)
+        # Configuration du système de grille de la fenêtre principale
+        self.grid_rowconfigure((0, 1), weight=1)
+        self.grid_columnconfigure(0, weight=1)
 
-        self.result_box = customtkinter.CTkTextbox(master=self, height=60, width=220)
-        self.result_box.configure(state="disabled")
-        self.result_box.grid(row=0, column=0,padx=5, sticky="w")
+        # Création et configuration du cadre pour afficher le résultat
+        self.frame_resultat = customtkinter.CTkFrame(self)
+        self.frame_resultat.grid(row=0, column=0, padx=5, pady=5, sticky="nwe")
 
-        self.button = customtkinter.CTkButton(self, text="C", fg_color= "#be4d25", width=60, height=60, command=lambda: self.button_callback("C"))
-        self.button.grid(row=0, column=1, padx=5, sticky="e")
+        # Configuration du système de grille pour le cadre de résultat
+        self.frame_resultat.grid_columnconfigure((0,1), weight=1)
+        self.frame_resultat.grid_rowconfigure(0, weight=1)
 
-        self.button_del = customtkinter.CTkButton(self, text="DEL", fg_color= "#be4d25", width=60, height=60, command=lambda: self.button_callback("DEL"))
-        self.button_del.grid(row=1, column=1, padx=5, pady=5, sticky="nw")
-        
-        self.button_division = customtkinter.CTkButton(self, text="/", fg_color= "#2596be", width=60, height=60, command=lambda: self.button_callback("/"))
-        self.button_division.grid(row=1, column=1, padx=0, pady=80, sticky="n")
+        # Zone de texte pour afficher le résultat
+        self.result_box = customtkinter.CTkTextbox(self.frame_resultat, height=60, width=220)
+        self.result_box.configure(state="disabled")  # Désactiver la zone de texte pour l'édition
+        self.result_box.grid(row=0, column=0,padx=5, pady=5, sticky="w")
 
-        self.button_soustraction = customtkinter.CTkButton(self, text="_", fg_color= "#2596be", width=60, height=60, command=lambda: self.button_callback("-"))
-        self.button_soustraction.grid(row=1, column=1, padx=0, pady=153, sticky="n")
+        # Bouton pour effacer le résultat
+        self.button = customtkinter.CTkButton(self.frame_resultat, text="C", fg_color= "#be4d25", width=60, height=60, command=lambda: self.button_callback("C"))
+        self.button.grid(row=0, column=1, padx=5,pady=5, sticky="e")
 
-        self.button_addition = customtkinter.CTkButton(self, text="+", fg_color= "#2596be", width=60, height=60, command=lambda: self.button_callback("+"))
-        self.button_addition.grid(row=1, column=1, padx=0, pady=102, sticky="s")
+        # Création et configuration du cadre pour les boutons de la calculatrice
+        self.frame_buttons = customtkinter.CTkFrame(self)
+        self.frame_buttons.grid(row=1, column=0, padx=5, pady=5, sticky="n")
 
-        self.button_egal = customtkinter.CTkButton(self, text="=", fg_color= "#96be25", width=60, height=60, command=self.button_callback_egal)
-        self.button_egal.grid(row=1, column=1, padx=0, pady=28, sticky="s")
+        # Configuration du système de grille pour le cadre des boutons
+        self.frame_buttons.grid_columnconfigure((0, 1, 2, 4), weight=1, pad=5)
+        self.frame_buttons.grid_rowconfigure((0, 1, 2, 3, 4), weight=1, pad=5)
 
-        self.frame = customtkinter.CTkFrame(self, height=150, width=240)
-        self.frame.grid(row=1, column=0, padx=5, sticky="nw")
+        self.create_buttons()
 
-        self.frame.grid_columnconfigure((0, 1, 2), weight=1, pad=5)
-        self.frame.grid_rowconfigure((0, 1, 2, 3, 4), weight=1, pad=5)
+    # Méthode pour créer et positionner les boutons de la calculatrice
+    def create_buttons(self):
+        buttons = [
+            ('(', 0, 0), (')', 0, 1), ('x²', 0, 2), ('DEL', 0, 3,'#be4d25'),
+            ('7', 1, 0), ('8', 1, 1), ('9', 1, 2), ('/', 1, 3,'#2596be'),
+            ('4', 2, 0), ('5', 2, 1), ('6', 2, 2), ('-', 2, 3,'#2596be'),
+            ('1', 3, 0), ('2', 3, 1), ('3', 3, 2), ('+', 3, 3,'#2596be'),
+            ('.', 4, 0), ('0', 4, 1), ('*', 4, 2,'#2596be'), ('=', 4, 3, '#96be25')
+        ]
 
-        self.button_ouv = customtkinter.CTkButton(self.frame, text="(", width=60, height=60, command=lambda: self.button_callback("("))
-        self.button_ouv.grid(row=0, column=0, padx=5, pady=5)
+        # Boucle pour créer chaque bouton et le positionner dans la grille
+        for button in buttons:
+            if len(button) == 4:
+                text, row, col, color = button
+            else:
+                text, row, col = button
+                color = "#1a6985"  # Couleur par défaut
 
-        self.button_fer = customtkinter.CTkButton(self.frame, text=")", width=60, height=60, command=lambda: self.button_callback(")"))
-        self.button_fer.grid(row=0, column=1, padx=5, pady=5)
+            self.create_button(text, row, col, color)
 
-        self.button_carre = customtkinter.CTkButton(self.frame, text="x²", width=60, height=60, command=lambda: self.button_callback("carre"))
-        self.button_carre.grid(row=0, column=2, padx=5, pady=5)
+    # Méthode pour créer un bouton avec les propriétés spécifiées
+    def create_button(self, text, row, col, color="#1a6985"):
+        button = customtkinter.CTkButton(self.frame_buttons, text=text, fg_color=color, width=60, height=60, font=('Arial', 18), command=lambda t=text: self.button_callback(t))
+        button.grid(row=row, column=col, padx=5, pady=5) 
 
-        self.button_sept = customtkinter.CTkButton(self.frame, text="7", width=60, height=60, command=lambda: self.button_callback(7))
-        self.button_sept.grid(row=1, column=0, padx=5, pady=5)
-
-        self.button_huit = customtkinter.CTkButton(self.frame, text="8", width=60, height=60, command=lambda: self.button_callback(8))
-        self.button_huit.grid(row=1, column=1, padx=5, pady=5)
-
-        self.button_neuf = customtkinter.CTkButton(self.frame, text="9", width=60, height=60, command=lambda: self.button_callback(9))
-        self.button_neuf.grid(row=1, column=2, padx=5, pady=5)
-
-        self.button_quatre = customtkinter.CTkButton(self.frame, text="4", width=60, height=60, command=lambda: self.button_callback(4))
-        self.button_quatre.grid(row=2, column=0, padx=5, pady=5)
-
-        self.button_cinq = customtkinter.CTkButton(self.frame, text="5", width=60, height=60, command=lambda: self.button_callback(5))
-        self.button_cinq.grid(row=2, column=1, padx=5, pady=5)
-
-        self.button_six = customtkinter.CTkButton(self.frame, text="6", width=60, height=60, command=lambda: self.button_callback(6))
-        self.button_six.grid(row=2, column=2, padx=5, pady=5)
-
-        self.button_un = customtkinter.CTkButton(self.frame, text="1", width=60, height=60, command=lambda: self.button_callback(1))
-        self.button_un.grid(row=3, column=0, padx=5, pady=5)
-
-        self.button_deux = customtkinter.CTkButton(self.frame, text="2", width=60, height=60, command=lambda: self.button_callback(2))
-        self.button_deux.grid(row=3, column=1, padx=5, pady=5)
-
-        self.button_trois = customtkinter.CTkButton(self.frame, text="3", width=60, height=60, command=lambda: self.button_callback(3))
-        self.button_trois.grid(row=3, column=2, padx=5, pady=5)
-
-        self.button_virgule = customtkinter.CTkButton(self.frame, text=".", width=60, height=60, command=lambda: self.button_callback("."))
-        self.button_virgule.grid(row=4, column=0, padx=5, pady=5)
-        
-        self.button_zero = customtkinter.CTkButton(self.frame, text="0", width=60, height=60, command=lambda: self.button_callback(0))
-        self.button_zero.grid(row=4, column=1, padx=5, pady=5)
-
-        self.button_mulplication = customtkinter.CTkButton(self.frame, fg_color= "#2596be", text="*", width=60, height=60, command=lambda: self.button_callback("*"))
-        self.button_mulplication.grid(row=4, column=2, padx=5, pady=5)
-
-
+    # Méthode de rappel pour les actions des boutons
     def button_callback(self, valeur):
-        print(f"button pressed {valeur}")
+        if valeur == '=':
+            self.button_callback_egal()
+        else:
+            print(f"button pressed {valeur}")
 
+    # Méthode de rappel pour le bouton égal
     def button_callback_egal(self):
-        print(f"button égal pressed")
+        print("button égal pressed")
 
+# Initialisation et démarrage de l'application
 app = Calculator()
 app.mainloop()
